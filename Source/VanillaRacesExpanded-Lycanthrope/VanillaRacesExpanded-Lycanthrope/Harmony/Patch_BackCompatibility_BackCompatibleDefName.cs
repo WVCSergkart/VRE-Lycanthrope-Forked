0f9f26a47788e7938e0ca67988e442db8a59f9b3
WVCSergkart <114881826+WVCSergkart@users.noreply.github.com>
2025-08-07 10:11:28 +0300

    speech inability replaced

---
+++ Source/VanillaRacesExpanded-Lycanthrope/VanillaRacesExpanded-Lycanthrope/Harmony/Patch_BackCompatibility_BackCompatibleDefName.cs
@@ -0,0 +1,47 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace VanillaRacesExpandedLycanthrope
+{
+
+    [HarmonyPatch(typeof(BackCompatibility), "BackCompatibleDefName")]
+    public static class Patch_BackCompatibility_BackCompatibleDefName
+    {
+        public static void Postfix(Type defType, string defName, ref string __result)
+        {
+            if (typeof(GeneDef).IsAssignableFrom(defType))
+            {
+                if (defName == "VRE_SpeechInability")
+                {
+                    __result = "WVC_AgeDebuff_Social";
+                }
+                //if (defName == "VRE_SpeechInability")
+                //{
+                //    __result = "WVC_AgeDebuff_Social";
+                //}
+                //if (defName == "VRE_SpeechInability")
+                //{
+                //    __result = "WVC_AgeDebuff_Social";
+                //}
+                //if (defName == "VRE_SpeechInability")
+                //{
+                //    __result = "WVC_AgeDebuff_Social";
+                //}
+                //if (defName == "VRE_SpeechInability")
+                //{
+                //    __result = "WVC_AgeDebuff_Social";
+                //}
+                //if (defName == "VRE_SpeechInability")
+                //{
+                //    __result = "WVC_AgeDebuff_Social";
+                //}
+            }
+        }
+    }
+
+}
